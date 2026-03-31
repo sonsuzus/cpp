907b715efc770fd15fb74a32536c37f88b77e872
Sonsuz Us <52765796+sonsuzus@users.noreply.github.com>
2026-03-31 13:06:44 +0300

    Create asalci.c to print prime numbers from 2 to 1000

---
+++ asalci.c
@@ -0,0 +1,21 @@
+'#include <stdio.h>
+
+// Function to check if a number is prime
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    printf("Prime numbers from 2 to 1000:\n");
+    for (int i = 2; i <= 1000; i++) {
+        if (is_prime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}'
